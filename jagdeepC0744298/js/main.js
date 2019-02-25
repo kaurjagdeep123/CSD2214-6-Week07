78f585665b14426cf91520c127f986f2a42cdb6a
visualstudio <visualstudio@230-PC18>
2019-02-25 15:37:11 -0500

    3.30

--- jagdeepC0744298/js/main.js
+++ jagdeepC0744298/js/main.js
@@ -1,1 +1,22 @@
 ﻿// Your code here!
+var bird = {
+    genus: "corvus",
+    species: "covex",
+    commonName: "raven",
+    callType: "squawky",
+    quote: "nevermore",
+    maxOffspring: 5,
+    noisy: true,
+    deadly: false
+};
+
+var bear = {
+    genus: "ursus",
+    species: "arctos",
+    commonName: "brown bear",
+    callType: "roar",
+    quote: "",
+    maxOffspring: 3,
+    noisy: true,
+    deadly: true
+};
